6987b43a141ac4ca7bc80c99297f06be713a1cd5
NicholasHubbard <NEHubbard1@student.fullsail.edu>
2020-02-14 17:37:38 -0900

    Code for the Web Server Assignment

---
+++ BootStrap Assignment/app.js
@@ -0,0 +1,29 @@
+var fs = require("fs");
+var http = require("http");
+var path = require("path"); 
+var url = require("url");
+
+
+http.createServer(function(req, res){
+
+	var parsed = url.parse(req.url);
+	var filename = path.parse(parsed.pathname);
+	
+	var filen = "";
+	if(filename.name == ""){
+		filen = "index"
+	}else{
+		filen = filename.name;
+	}
+	
+
+	fs.readFile(filen + ".html", function(err, data){
+
+		res.writeHead(200);
+		res.write("<script>var name = '';</script>");
+		res.end(data);
+
+	})
+
+
+}).listen("8080")
