ca9abb5b1e70b67300b0f6ccd4f050ca336fad0a
NicholasHubbard <NEHubbard1@student.fullsail.edu>
2020-05-11 18:33:36 -0800

    fixed issues with user input

--- rubyGrader.rb
+++ rubyGrader.rb
@@ -1,6 +1,18 @@
 # Nicholas Hubbard
 # Assignment Ruby Grader
 
+# User inputs for name, assignment, and grade
+puts "Enter your name: "
+userName = gets
+
+puts "Enter your assignment name: "
+userAssignment = gets
+
+puts "Enter your grade in number form: "
+userGrade = gets.chomp.to_i
+
+
+# class to initialize and define object
 class Grader 
 	def initialize(name, assignment, grade)
 		@name = name
@@ -20,47 +32,33 @@ class Grader
 		@grade
 	end
 
+	# method to convert grade into a letter grade
+	def convertGrade(grade)
+		if grade < 60
+			puts "Final Grade: F"
+		elsif grade >= 60 && grade < 70
+			puts "Final Grade: D"
+		elsif grade >= 70 && grade < 80
+			puts "Final Grade: C"
+		elsif grade >= 80 && grade < 90
+			puts "Final Grade: B"
+		elsif grade >= 90 && grade < 100
+			puts "Final Grade: A"
+		elsif grade == 100
+			puts "Final Grade: A+"
+		elsif grade > 100
+			puts "Final Grade: A++ Way to go way overboard haha"
+		end
+	end
+
 end
 
-def convertGrade(score)
-	if score < 60
-		return "F"
-	elsif score >= 60 && score < 70
-		return "D"
-	elsif score >= 70 && score < 80
-		return "C"
-	elsif score >= 80 && score < 90
-		return "B"
-	elsif score >= 90 && score < 100
-		return "A"
-	elsif score == 100
-		return "A+"
-	elsif score > 100
-		return "A++ Way to go way overboard haha"
-end
-
-puts "Enter your name: "
-userName = gets
-
-puts "Enter your assignment name: "
-userAssignment = gets
-
-puts "Enter your grade in number form: "
-userGrade = gets
-
-
+# instantiating myUser for create the actual object
 myUser = Grader.new(userName, userAssignment, userGrade)
 
-myUser.score = convertGrade(myUser.grade)
-
+# printing to the console to show object name, assignment, and method to show letter grade
 puts "Student Name: #{myUser.name}"
 
 puts "Assignment graded: #{myUser.assignment}"
 
-puts "Final Grade: #{myUser.score}"
-
-
-
-
-
-end	
+myUser.convertGrade(myUser.grade)
